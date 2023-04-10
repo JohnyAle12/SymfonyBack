1466b52cccb793f9c3234e861cb3e6d68c471e04
Johny Prieto <johnyprieto001@gmail.com>
2023-04-09 19:50:08 -0500

    add some validation to request

--- src/Controller/Api/UserController.php
+++ src/Controller/Api/UserController.php
@@ -10,12 +10,18 @@ use FOS\RestBundle\Controller\Annotations\{Delete, Get, Post, Put, Patch};
 use Symfony\Component\HttpFoundation\Request;
 use FOS\RestBundle\Controller\Annotations\View as ViewAttribute;
 use Doctrine\ORM\EntityManagerInterface;
+use Exception;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+use Symfony\Component\Validator\Validator\ValidatorInterface;
+use Symfony\Component\HttpFoundation\Response;
+use Throwable;
 
 class UserController extends AbstractFOSRestController
 {
     public function __construct(
         private LoggerInterface $logger,
+        private ValidatorInterface $validator,
         private UserPasswordHasherInterface $passwordHasher
     ){}
 
@@ -33,18 +39,46 @@ class UserController extends AbstractFOSRestController
     public function store(
         Request $request,
         EntityManagerInterface $em
-    ): User
+    )
     {
-        $data = $request->toArray();
-        $user = new User;
-        $user->setName($data['name']);
-        $user->setEmail($data['email']);
-        $user->setPassword($this->passwordHasher->hashPassword(
-            $user, $data['password']
-        ));
-        $em->persist($user);
-        $em->flush();
+        try {
+            $data = $request->toArray();
+            $user = new User;
+            $user->setName($data['name']);
+            $user->setEmail($data['email']);
+            $user->setPassword($this->passwordHasher->hashPassword(
+                $user, $data['password']
+            ));
 
-        return $user;
+            $errors = $this->validateRequest($user);
+            if(count($errors)){
+                return new JsonResponse([
+                    'errors' => $errors
+                ]);
+            }
+
+            $em->persist($user);
+            $em->flush();
+
+            return $user;
+        } catch (\Throwable $th) {
+            return new JsonResponse([
+                'error' => $th->getMessage()
+            ]);
+        }
+    }
+
+    private function validateRequest(
+        User $user,
+    ): array
+    {
+        $messages = [];
+        $errors = $this->validator->validate($user);
+        if(count($errors) > 0){
+            foreach ($errors as $violation) {
+                $messages[] = $violation->getMessage();
+            }
+        }
+        return $messages;
     }
 }
